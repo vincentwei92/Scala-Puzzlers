116db6a0509aebfa099b61d24d17872e7a0bceaa
Vincent Wei <weshpolo@live.com>
2016-03-26 20:32:26 +0800

    rev

--- Puzzler2.scala
+++ Puzzler2.scala
@@ -1,7 +1,12 @@
 object Puzzler {
 	val MONTH = 2; val DAY = 100;
 	val SECOND = 12; val HOUR = 24; val MINUTE = 30;
+
 	var (HOUR, MINUTE, SECOND) = (12, 0, 0)
+	// note the differences
+	val proxy: Any = (12, 0, 0)
+	var (HOUR, MINUTE, SECOND) = (12, 0, 0)
+
 	val (HOUR, MINUTE, SECOND) = (1,2,3)
 
 	var (x, y, z) = (1, 2, 3)
--- Puzzler8.scala
+++ Puzzler8.scala
@@ -1,7 +1,7 @@
 object Puzzler {
 	val xs = Seq(Seq("a", "b", "c"), Seq("d", "e", "f"), Seq("g", "h"), Seq("i", "j", "k"))
 	// runtime not compiler time
-	val yuuuuuuuuuuuus = for (List(x, y, z,k) <- xs) yield x + y + z
+	// val y = for (List(x, y, z,k) <- xs) yield x + y + z
 	val ys = for (Seq(x, y, z) <- xs) yield x + y + z
 	val zs = xs map { case Seq(x, y, z) => x + y + z }
 
